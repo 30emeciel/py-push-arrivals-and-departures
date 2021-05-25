ce8c60ff4624fc093a6081f6f994baa30a28a6da
Tony <tony.lbvre@gmail.com>
2021-05-25 15:01:42 +0200

    feat: upd to core 4 + add arrival times and fix when there is no one

--- main.py
+++ main.py
@@ -1,20 +1,15 @@
 import logging
 from datetime import datetime, timezone, timedelta
 
-import firebase_admin
 from box import Box
-from core.notification_message_templates import generate_notification_message, ARRIVALS_AND_DEPARTURES
-from core.slack_message import send_slack_message
-from firebase_admin import credentials
-from firebase_admin import firestore
+from core import firestore_client
+from core.slack_message import SlackSender
+from core.tpl import render
 
-# Use the application default credentials
-cred = credentials.ApplicationDefault()
-firebase_admin.initialize_app(cred, {
-    'projectId': "trentiemeciel",
-})
+PARIS_TZ = timezone(timedelta(hours=2))
 
-db = firestore.client()
+db = firestore_client.db()
+slack_sender = SlackSender()
 
 log = logging.getLogger(__name__)
 
@@ -32,8 +27,12 @@ def from_pubsub(event, context):
 
 def push_arrivals_and_departures():
     # TODO: fix timestamp in DB!
-    today = datetime.now(tz=timezone(timedelta(hours=2))) \
+    today = datetime.now(tz=PARIS_TZ) \
         .replace(hour=0, minute=0, second=0, microsecond=0)
+    push_arrivals_and_departures_sub(today)
+
+
+def push_arrivals_and_departures_sub(today):
     request_arrivals = db.collection_group('requests') \
         .where('state', "==", "CONFIRMED") \
         .where('arrival_date', '==', today) \
@@ -43,18 +42,24 @@ def push_arrivals_and_departures():
         .where('departure_date', '==', today) \
         .stream()
 
-    def request_to_pax_name(doc):
-        return Box(doc.reference.parent.parent.get().to_dict()).name
+    def reservation_data_with_time(r):
+        pax = Box(r.reference.parent.parent.get().to_dict())
+        r = Box(r.to_dict(), default_box=True, default_box_attr="")
+        return f"{pax.name} ({r.arrival_time})"
 
-    def requests_to_pax_names(query):
-        return (request_to_pax_name(doc) for doc in query)
+    def reservation_data(r):
+        pax = Box(r.reference.parent.parent.get().to_dict())
+        return f"{pax.name}"
 
-    arrivals = requests_to_pax_names(request_arrivals)
-    departures = requests_to_pax_names(request_departures)
+    def reservation_query_to_data(query):
+        return [reservation_data_with_time(r) for r in query]
 
+    arrivals = reservation_query_to_data(request_arrivals)
+    departures = [reservation_data(r) for r in request_departures]
     data = {
         "arrivals": arrivals,
         "departures": departures
     }
-    notification_message = generate_notification_message(ARRIVALS_AND_DEPARTURES, data)
-    send_slack_message(notification_message)
+    slack_message = render("arrivals_and_departures_fr.txt", data).strip()
+    if slack_message:
+        slack_sender.send_slack_message(slack_message)
--- test_main.py
+++ test_main.py
@@ -1,17 +1,24 @@
 import logging
-from unittest import TestCase
+from datetime import datetime
+
+import pytest
 
 import dotenv
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-class Test(TestCase):
+@pytest.fixture(autouse=True)
+def setup():
+    dotenv.load_dotenv()
 
-    def setUp(self):
-        dotenv.load_dotenv()
 
-    def test_push_arrivals_and_departures(self):
-        import main
-        main.push_arrivals_and_departures()
+def test_push_arrivals_and_departures():
+    import main
+    main.push_arrivals_and_departures()
 
+
+def test_push_arrivals_and_departures_sub():
+    import main
+    today = datetime(2021, 5, 24, 00, 00, 00, tzinfo=main.PARIS_TZ)
+    main.push_arrivals_and_departures_sub(today)
